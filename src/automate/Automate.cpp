e6cb543698ed35a376ab43409924173de0c61ca4
Haidara Mohamed El Mouctar <mohamed.haidara@insa-lyon.fr>
2016-03-07 08:50:28 +0100

    push et pop de l'automate

--- src/automate/Automate.cpp
+++ src/automate/Automate.cpp
@@ -6,3 +6,45 @@ void Automate::decalage(Etat * etat)
     Etat * e = new Etat0();
     std::cout << e << std::endl;
 }
+
+Etat* Automate::popEtat()
+{
+    Etat* etat = pilesEtats.top();
+    pilesEtats.pop();
+    return etat;
+}
+
+Symbole* Automate::popSymbole()
+{
+    Symbole * symbole = pilesSymboles.top();
+    pilesSymboles.pop();
+    return symbole;
+}
+
+void Automate::pushEtat(Etat * const etat)
+{
+    pilesEtats.push(etat);
+}
+
+void Automate::pushSymbole(Symbole * const symbole)
+{
+    pilesSymboles.push(symbole);
+}
+
+void Automate::popEtat(int nb)
+{
+    for (int i = 0; i < nb; ++i)
+    {
+        pilesEtats.pop();
+    }
+}
+
+
+
+
+
+
+
+
+
+
--- src/automate/Automate.h
+++ src/automate/Automate.h
@@ -2,13 +2,35 @@
 #define LUTIN_AUTOMATE_H
 
 
+#include <stack>
 #include <Etat.h>
+#include <Symbole.h>
+
 class Etat;
+
 class Automate
 {
 public:
-    void decalage(Etat * etat);
+
+
+
+    void decalage(Etat *etat);
+
+    Symbole *popSymbole();
+
+    Etat *popEtat();
+
+    void pushEtat(Etat *const etat);
+
+    void pushSymbole(Symbole *const symbole);
+
+    void popEtat(int nb);
+
 private:
+    std::stack<Etat *> pilesEtats;
+
+    std::stack<Symbole *> pilesSymboles;
+
 
 };
 
