7cea1e6a0330c7720c6dbdacf530e01f33564da4
robinroyer <robin.royer974@gmail.com>
2016-03-07 09:51:42 +0100

    adding unfollow files

---
+++ src/symboles/ErreurLexicale.cpp
@@ -0,0 +1,1 @@
+#include "ErreurLexicale.h"
---
+++ src/symboles/ErreurLexicale.h
@@ -0,0 +1,15 @@
+#ifndef LUTIN_ERREURLEXICALE_H
+#define LUTIN_ERREURLEXICALE_H
+
+#include "Symbole.h"
+
+
+class ErreurLexicale : public Symbole
+{
+public:
+    ErreurLexicale();
+    ~ErreurLexicale();
+};
+
+
+#endif //LUTIN_ERREURLEXICALE_H
