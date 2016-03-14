70abb8678978da182c7deddf6afc401bf38e156f
Loïc Touzard <loic.touzard@gmail.com>
2016-03-15 00:39:58 +0100

    ajout des transitions pour les variable multiple sur une ligne --> partie declaration de l'automate terminée

--- src/etats/Etat14.cpp
+++ src/etats/Etat14.cpp
@@ -1,7 +1,30 @@
+#include <IdTerminal.h>
+#include <IdentificateurVariable.h>
 #include "Etat14.h"
 
 int Etat14::transition(Automate *automate, Symbole *s) {
     switch (*s) {
+        case VIRGULE_TERMINAL:
+        case POINT_VIRGULE_TERMINAL: {
+            //On a lu un suivant de IDS
+            IdTerminal *id = (IdTerminal *) automate->popSymbole();
+            automate->popSymbole(); // pop du VIRGULE_TERMINAL
+            IdentificateurVariable *ids = (IdentificateurVariable *) automate->popSymbole(); // IDS
+            automate->popEtat(3);   // pop de 3 symboles, donc pop de 3 Etats, retour en E4
+
+            // Etat courant : Etat4
+            IdentificateurVariable *identificateurConstante = new IdentificateurVariable(id);
+
+            // il faut insérer identificateurConstante à la fin de la file d'IdentificateurVariable.
+            // on récupère la derniere IdentificateurVariable
+            IdentificateurVariable *dernierIdentificateur = ids;
+            while (dernierIdentificateur->getSuivant() != nullptr) {
+                dernierIdentificateur = dernierIdentificateur->getSuivant();
+            }
+            // insertion dans la liste
+            dernierIdentificateur->setSuivant(identificateurConstante);
+            return automate->etatCourant()->transition(automate, ids);
+        }
         default:
             return ERREUR;
     }
