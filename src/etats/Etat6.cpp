4876fc1f9dc320c8857b5181f2a085e38d5ad198
Loïc Touzard <loic.touzard@gmail.com>
2016-03-19 16:33:49 +0100

    Correction Etat6, transition pour "Ecrire a;" fonctionnelle

--- src/etats/Etat6.cpp
+++ src/etats/Etat6.cpp
@@ -1,3 +1,4 @@
+#include <Expression.h>
 #include "Etat6.h"
 #include "Etat31.h"
 #include "Etat30.h"
@@ -6,17 +7,45 @@
 
 
 int Etat6::transition(Automate *automate, Symbole *s) {
-    switch (*s) {
+    // ATTENTION CONFLIT DE TYPE POTENTIEL DANS LES CASES
+    int intS = (int)*s;
+    switch (intS){
+        case EXPRESSION:
+        case EXPRESSION_ADDITIVE:
+        case EXPRESSION_SOUSTRACTIVE:
+        case TERME:
+        case EXPRESSION_MULT:
+        case EXPRESSION_DIV:
+        case FACTEUR:
+        case CONSTANTE_NUMERIQUE:
+        case IDENTIFICATEUR_FACTEUR:
+        case EXPRESSION_PARENTHESEE:
+            intS = ((Expression*)s)->identifiantDynamique();
+            break;
+        default:
+            break;
+    }
+
+    switch (intS) {
         case ID_TERMINAL:
             automate->decalage(new Etat31, s);
             return CONTINUE;
         case FACTEUR:
-            automate->decalage(new Etat30, s);
+        case CONSTANTE_NUMERIQUE:
+        case IDENTIFICATEUR_FACTEUR:
+        case EXPRESSION_PARENTHESEE:
+            automate->pushSymbole(s);
+            automate->pushEtat(new Etat30);
             return CONTINUE;
         case TERME:
-            automate->decalage(new Etat17, s);
+        case EXPRESSION_MULT:
+        case EXPRESSION_DIV:
+            automate->pushSymbole(s);
+            automate->pushEtat(new Etat17);
             return CONTINUE;
         case EXPRESSION:
+        case EXPRESSION_ADDITIVE:
+        case EXPRESSION_SOUSTRACTIVE:
             automate->pushSymbole(s);
             automate->pushEtat(new Etat16);
             return CONTINUE;
