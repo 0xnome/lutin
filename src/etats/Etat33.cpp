3a6705b1120058ca69a4c0dccb9e3b0af867dc17
HAIDARA Mohamed El Mouctar <elmhaidara@gmail.com>
2016-03-19 18:33:04 +0100

    fix 39

--- src/etats/Etat33.cpp
+++ src/etats/Etat33.cpp
@@ -1,21 +1,57 @@
+#include <Expression.h>
 #include "Etat33.h"
 #include "Etat31.h"
 #include "Etat32.h"
 #include "Etat39.h"
 #include "Etat17.h"
+#include "Etat30.h"
+#include "Etat29.h"
 
 int Etat33::transition(Automate *automate, Symbole *s)
 {
-    switch (*s)
-    {
+
+    // ATTENTION CONFLIT DE TYPE POTENTIEL DANS LES CASES
+    int intS = (int)*s;
+    switch (intS){
         case EXPRESSION:
+        case EXPRESSION_ADDITIVE:
+        case EXPRESSION_SOUSTRACTIVE:
+        case TERME:
+        case EXPRESSION_MULT:
+        case EXPRESSION_DIV:
+        case FACTEUR:
+        case CONSTANTE_NUMERIQUE:
+        case IDENTIFICATEUR_FACTEUR:
+        case EXPRESSION_PARENTHESEE:
+            intS = ((Expression*)s)->identifiantStatique();
+            break;
+        default:
+            break;
+    }
+
+
+    switch (intS)
+    {
+        case FACTEUR:
+        case CONSTANTE_NUMERIQUE:
+        case IDENTIFICATEUR_FACTEUR:
+        case EXPRESSION_PARENTHESEE:
             automate->pushSymbole(s);
-            automate->pushEtat(new Etat39);
+            automate->pushEtat(new Etat30);
             return CONTINUE;
         case TERME:
+        case EXPRESSION_MULT:
+        case EXPRESSION_DIV:
             automate->pushSymbole(s);
             automate->pushEtat(new Etat17);
             return CONTINUE;
+        case EXPRESSION:
+        case EXPRESSION_ADDITIVE:
+        case EXPRESSION_SOUSTRACTIVE:
+            automate->pushSymbole(s);
+            automate->pushEtat(new Etat39);
+            return CONTINUE;
+
 
         case ID_TERMINAL :
             automate->decalage(new Etat31, s);
