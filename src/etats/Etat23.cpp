5ec8df7e3cbf137abf55b4dfa8daa60d38d7e848
robinroyer <robin.royer974@gmail.com>
2016-03-19 17:53:19 +0100

    transition etat23

--- src/etats/Etat23.cpp
+++ src/etats/Etat23.cpp
@@ -4,14 +4,52 @@
 
 int Etat23::transition(Automate *automate, Symbole *s)
 {
-    switch (*s)
+    // ATTENTION CONFLIT DE TYPE POTENTIEL DANS LES CASES
+    int intS = (int)*s;
+    switch (intS){
+        case EXPRESSION:
+        case EXPRESSION_ADDITIVE:
+        case EXPRESSION_SOUSTRACTIVE:
+        case TERME:
+        case EXPRESSION_MULT:
+        case EXPRESSION_DIV:
+        case FACTEUR:
+        case CONSTANTE_NUMERIQUE:
+        case IDENTIFICATEUR_FACTEUR:
+        case EXPRESSION_PARENTHESEE:
+            intS = ((Expression*)s)->identifiantStatique();
+            break;
+        default:
+            break;
+    }
+
+    switch (intS)
     {
         case ID_TERMINAL:
             automate->decalage(new Etat31, s);
             return CONTINUE;
+
         case TERME:
+        case EXPRESSION_MULT:
+        case EXPRESSION_DIV:
             automate->pushSymbole(s);
             automate->pushEtat(new Etat29);
+            return CONTINUE;
+
+        case FACTEUR:
+        case CONSTANTE_NUMERIQUE:
+        case IDENTIFICATEUR_FACTEUR:
+        case EXPRESSION_PARENTHESEE:
+            automate->pushSymbole(s);
+            automate->pushEtat(new Etat30);
+            return CONTINUE;
+
+        case PAROUV_TERMINAL:
+            automate->decalage(new Etat33, s);
+            return CONTINUE;
+
+        case NUM_TERMINAL:
+            automate->decalage(new Etat32, s);
             return CONTINUE;
         default:
             return ERREUR;
