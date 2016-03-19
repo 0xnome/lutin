058d1b1fe2871967c2b4ed6f4f2c9efe607acd10
robinroyer <robin.royer974@gmail.com>
2016-03-19 14:56:31 +0100

    wip

--- src/etats/Etat17.cpp
+++ src/etats/Etat17.cpp
@@ -1,7 +1,19 @@
 #include "Etat17.h"
+#include "Terme.h"
 
 int Etat17::transition(Automate *automate, Symbole *s) {
     switch (*s) {
+        case POINT_VIRGULE_TERMINAL:
+        case PARFER_TERMINAL:
+        case EXPRESSION_SOUSTRACTIVE:
+        case EXPRESSION_ADDITIVE:
+        {
+            Terme * terme = (Terme*) automate->popSymbole();
+            automate->popEtat(1);
+            Expression* expression = (Expression*) terme;
+
+            return automate->etatCourant()->transition(automate, expression);
+        }
         default:
             return ERREUR;
     }
--- src/etats/Etat6.cpp
+++ src/etats/Etat6.cpp
@@ -1,7 +1,24 @@
 #include "Etat6.h"
+#include "Etat31.h"
+#include "Etat30.h"
+#include "Etat17.h"
+#include "Etat16.h"
+
 
 int Etat6::transition(Automate *automate, Symbole *s) {
     switch (*s) {
+        case ID_TERMINAL:
+            automate->decalage(new Etat31, s);
+            return CONTINUE;
+        case FACTEUR:
+            automate->decalage(new Etat30, s);
+            return CONTINUE;
+        case TERME:
+            automate->decalage(new Etat17, s);
+            return CONTINUE;
+        case EXPRESSION:
+            automate->decalage(new Etat16, s);
+            return CONTINUE;
         default:
             return ERREUR;
     }
