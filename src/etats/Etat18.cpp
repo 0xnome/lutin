fb164cf2067b9d3be2afa47c437d92c5c4341776
robinroyer <robin.royer974@gmail.com>
2016-03-19 18:57:08 +0100

    transition ettat 18 et 25

--- src/etats/Etat18.cpp
+++ src/etats/Etat18.cpp
@@ -1,6 +1,8 @@
 #include "Expression.h"
 #include "Etat18.h"
 #include "Etat31.h"
+#include "Etat32.h"
+#include "Etat33.h"
 #include "Etat30.h"
 #include "Etat17.h"
 #include "Etat25.h"
@@ -26,11 +28,11 @@ int Etat18::transition(Automate *automate, Symbole *s) {
     }
 
     switch (intS) {
-        // TODO terminal : num, parouv
         // TODO non terminal : EXP, TERME
         case ID_TERMINAL:
             automate->decalage(new Etat31, s);
             return CONTINUE;
+
         case FACTEUR:
         case CONSTANTE_NUMERIQUE:
         case IDENTIFICATEUR_FACTEUR:
@@ -38,18 +40,29 @@ int Etat18::transition(Automate *automate, Symbole *s) {
             automate->pushSymbole(s);
             automate->pushEtat(new Etat30);
             return CONTINUE;
+
         case TERME:
         case EXPRESSION_MULT:
         case EXPRESSION_DIV:
             automate->pushSymbole(s);
             automate->pushEtat(new Etat17);
             return CONTINUE;
+
         case EXPRESSION:
         case EXPRESSION_ADDITIVE:
         case EXPRESSION_SOUSTRACTIVE:
             automate->pushSymbole(s);
             automate->pushEtat(new Etat25);
             return CONTINUE;
+
+        case NUM_TERMINAL:
+            automate->decalage(new Etat32, s);
+            return CONTINUE;
+
+        case PAROUV_TERMINAL:
+            automate->decalage(new Etat33, s);
+            return CONTINUE;
+
         default:
             return ERREUR;
     }
--- src/etats/Etat25.cpp
+++ src/etats/Etat25.cpp
@@ -1,11 +1,22 @@
 #include "Etat25.h"
+#include "Etat23.h"
+#include "Etat24.h"
 #include "Etat35.h"
 
 int Etat25::transition(Automate *automate, Symbole *s) {
     switch (*s) {
+        case EXPRESSION_ADDITIVE:
+            automate->decalage(new Etat23, s);
+            return CONTINUE;
+
+        case EXPRESSION_SOUSTRACTIVE:
+            automate->decalage(new Etat24, s);
+            return CONTINUE;
+
         case POINT_VIRGULE_TERMINAL:
             automate->decalage(new Etat35, s);
             return CONTINUE;
+
         default:
             return ERREUR;
     }
