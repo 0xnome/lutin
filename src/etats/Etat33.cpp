8c1a4e773c08786fae0b17f114c0b9c6666ce2f8
HAIDARA Mohamed El Mouctar <elmhaidara@gmail.com>
2016-03-19 18:16:21 +0100

    transition de 33 vers 39 par EXPRESSION et de 33 vers 17 par TERME

--- src/etats/Etat33.cpp
+++ src/etats/Etat33.cpp
@@ -1,11 +1,22 @@
 #include "Etat33.h"
 #include "Etat31.h"
 #include "Etat32.h"
+#include "Etat39.h"
+#include "Etat17.h"
 
 int Etat33::transition(Automate *automate, Symbole *s)
 {
     switch (*s)
     {
+        case EXPRESSION:
+            automate->pushSymbole(s);
+            automate->pushEtat(new Etat39);
+            return CONTINUE;
+        case TERME:
+            automate->pushSymbole(s);
+            automate->pushEtat(new Etat17);
+            return CONTINUE;
+
         case ID_TERMINAL :
             automate->decalage(new Etat31, s);
             return CONTINUE;
