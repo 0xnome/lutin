e720b63bd8dcdf5a6ab8b3268d38a05ee3747aaf
HAIDARA Mohamed El Mouctar <elmhaidara@gmail.com>
2016-03-19 17:41:42 +0100

    transition des terminaux à partir de l'Etat33

--- src/etats/Etat33.cpp
+++ src/etats/Etat33.cpp
@@ -1,7 +1,20 @@
 #include "Etat33.h"
+#include "Etat31.h"
+#include "Etat32.h"
 
-int Etat33::transition(Automate *automate, Symbole *s) {
-    switch (*s) {
+int Etat33::transition(Automate *automate, Symbole *s)
+{
+    switch (*s)
+    {
+        case ID_TERMINAL :
+            automate->decalage(new Etat31, s);
+            return CONTINUE;
+        case NUM_TERMINAL:
+            automate->decalage(new Etat32, s);
+            return CONTINUE;
+        case PAROUV_TERMINAL:
+            automate->decalage(new Etat33, s);
+            return CONTINUE;
         default:
             return ERREUR;
     }
