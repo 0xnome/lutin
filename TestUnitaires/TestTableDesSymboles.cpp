5c425e124e43eb61c85ea2e899967344c1ca9123
HAIDARA Mohamed El Mouctar <elmhaidara@gmail.com>
2016-03-21 11:21:54 +0100

    nouveau test pour la table des symboles

--- TestUnitaires/TestTableDesSymboles.cpp
+++ TestUnitaires/TestTableDesSymboles.cpp
@@ -6,21 +6,24 @@
 
 using namespace std;
 
+const unsigned ligne = 0;
+const unsigned colonne = 0;
+
 void testAjouterConstante()
 {
     TableDesSymboles tableDesSymboles;
 
     string constString1("constVar");
 
-  //  tableDesSymboles.ajouterConstante(constString1, 46);
+    tableDesSymboles.ajouterConstante(constString1, ligne, colonne, 46);
     assert(tableDesSymboles.getNbConstante() == 1);
-    assert(tableDesSymboles.getConstanteValeur(constString1) == 46);
+    assert(*tableDesSymboles.getConstanteValeur(constString1) == 46);
 
 
     // ajout d'une constante de mm nom
     //tableDesSymboles.ajouterConstante(constString1, 42);
     assert(tableDesSymboles.getNbConstante() == 1);
-    assert(tableDesSymboles.getConstanteValeur(constString1) == 46);
+    assert(*tableDesSymboles.getConstanteValeur(constString1) == 46);
 
 
 }
@@ -30,10 +33,9 @@ void testEstUtiliseeConstante()
     TableDesSymboles tableDesSymboles;
     string constString1("constVar");
 
- //   tableDesSymboles.ajouterConstante(constString1, 46);
+    tableDesSymboles.ajouterConstante(constString1,ligne,colonne, 46);
     tableDesSymboles.setConstanteUtilisee(constString1);
 
-    //assert(tableDesSymboles.)
 
 }
 
@@ -42,12 +44,24 @@ void testEstInitialiseeEstUtiliseeVariable()
     TableDesSymboles tableDesSymboles;
     string variable("variable");
 
-//    tableDesSymboles.ajouterVariable(variable);
+    tableDesSymboles.ajouterVariable(variable,ligne,colonne);
 
     assert(tableDesSymboles.getNbVariable() == 1);
 
     assert(!tableDesSymboles.estInitialisee(variable));
     assert(!tableDesSymboles.estUtiliseeVariable(variable));
+
+    assert(tableDesSymboles.getVariableValeur(variable) == nullptr);
+    tableDesSymboles.setVariableUtilisee(variable);
+    assert(tableDesSymboles.estUtiliseeVariable(variable));
+
+    tableDesSymboles.setVariableValeur(variable,42);
+    assert(*tableDesSymboles.getValeur(variable) == 42);
+    assert(tableDesSymboles.estInitialisee(variable));
+
+    tableDesSymboles.setInitialisee(variable);
+    assert(tableDesSymboles.getValeur(variable) == nullptr);
+    assert(tableDesSymboles.estInitialisee(variable));
 
 }
 
@@ -57,19 +71,24 @@ void testAjouterVariable()
     TableDesSymboles tableDesSymboles;
     string variable("variable");
 
-//    tableDesSymboles.ajouterVariable(variable);
+    tableDesSymboles.ajouterVariable(variable,ligne,colonne);
 
     assert(tableDesSymboles.getNbVariable() == 1);
 
-    tableDesSymboles.setVariableValeur(variable,42);
-    assert(tableDesSymboles.getVariableValeur(variable) == 42);
+    tableDesSymboles.setVariableValeur(variable, 42);
+    //assert(tableDesSymboles.getVariableValeur(variable) == 42);
 }
 
 
+/**
+ *
+ */
 void testEstDeclaree()
 {
     TableDesSymboles tableDesSymboles;
 }
+
+
 
 int main(int argc, char **argv)
 {
